0db00ed65e9b4d1c6b3d512f708df2600c517a39
edwinjhlee <edwin.jh.lee@gmail.com>
2019-08-06 14:05:39 +0800

    rm

--- src/run/ts.ts
+++ src/run/ts.ts
@@ -3,7 +3,6 @@ import { compile } from "../tseval"
 export async function run(content_str: string){
     try{
         const code = compile(content_str)
-        console.log(compile)
         await eval(code)
     } catch (err) {
         console.log(err)
