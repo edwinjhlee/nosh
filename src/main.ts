2da2c11870022a7a9b580b61b7719f983a49fc1f
edwinjhlee <edwin.jh.lee@gmail.com>
2019-08-20 01:25:55 +0800

    Fix argument problem

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,15 @@ import fs from "fs"
     const filepath = process.argv[2]
     const str = fs.readFileSync(filepath).toString()
 
+    process.argv.shift()
+
     switch (process.env["ENGINE"]) {
         case "js":  return require("./run/js").run(str)
         case "ts":  return require("./run/ts").run(str)
         default:    return require("./run/auto").run(str)
+        // TODO: why not? require is blocking, but efficient
+        // case "js":  return (await import("./run/js")).run(str)
+        // case "ts":  return (await import("./run/ts")).run(str)
+        // default:    return (await import("./run/auto")).run(str)
     }
 })()
